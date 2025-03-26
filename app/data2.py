a3bcb003336935e0af43d08796014cb6e046e3ee
Fjor Robles <126599236+Fjorrobles@users.noreply.github.com>
2025-03-25 20:54:51 -0700

    Update data2.py

--- app/data2.py
+++ app/data2.py
@@ -1,12 +1,16 @@
+"""
+Fjor Robles 
+"""
 import requests
 from bs4 import BeautifulSoup
 import json
 
 def web_scraper():
-    url = 'https://starbucksmenuprice.net'  # URL to scrape from
-    data_file = "./app/data/menu.json"  # Path to save the JSON data
+    """ function to get data from a specific website """
+    url = 'https://starbucksmenuprice.net'  
+    #json data file path in folder
+    data_file = "./app/data/menu.json" 
 
-    # Sending GET request to the website
     response = requests.get(url)
     
     # Check if the request was successful
@@ -16,16 +20,18 @@ def web_scraper():
         # List to store the scraped data
         data = []
         
-        # Extracting all 'strong' elements (or adjust this if needed)
+        # Extracting all 'strong' elements
         name_elements = soup.find_all('strong')
         
         for name in name_elements:
             # Extract the text content of the 'strong' element
             name_text = name.get_text(strip=True)
             
-            if name_text:  # Avoid adding empty strings
+            if name_text:  
                 data.append(name_text)
+        #skip first five elements of data from webpage
         data = data[5:]
+        
         # Write the data to a JSON file
         with open(data_file, 'w') as f:
             json.dump(data, f, indent=4)
@@ -35,7 +41,7 @@ def web_scraper():
         print(f"Failed to retrieve data. Status code: {response.status_code}")
         return []
 
-# Example usage
+
 if __name__ == "__main__":
     scraped_data = web_scraper()
     if scraped_data:
