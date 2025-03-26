8a55691af8532cf03c54e311311d36888bd93c59
Fjor Robles <126599236+Fjorrobles@users.noreply.github.com>
2025-03-25 20:58:06 -0700

    Update json_view.py

--- app/json_view.py
+++ app/json_view.py
@@ -1,10 +1,15 @@
+"""
+Fjor Robles
+"""
 import streamlit as st
 from data2 import web_scraper
 
 st.markdown("# JSON View 🫘")
 st.sidebar.markdown("# JSON View 🫘")
 
+#point to the data file with the scraped information
 data_file = "./app/data/menu.json" 
+#call function
 menu = web_scraper()
 
 st.title("Strarbucks Menu data")
